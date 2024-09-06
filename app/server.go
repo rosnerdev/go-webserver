0b20bfa599d4cf44317f11a240dceed14a33dad0
rosnerdev <rosneryahav@gmail.com>
2024-09-07 00:05:13 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -9,13 +9,21 @@ import (
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		log.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		log.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	toWrite := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	_, err = conn.Write(toWrite)
+	if err != nil {
+		log.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
 }
